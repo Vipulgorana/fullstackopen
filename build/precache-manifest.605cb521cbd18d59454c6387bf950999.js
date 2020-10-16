991c9f47f859685e592b21506ffa705c0ed006c2
= <vipulgorana@gmail.com>
2020-10-16 13:21:37 +0530

    practcie

---
+++ build/precache-manifest.605cb521cbd18d59454c6387bf950999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c844081cfc3e3c965a0cb9ee1ee7e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e120999074ad74dc324",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d534018b83763cc22d39",
+    "url": "/static/js/2.e06c31aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e06c31aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e120999074ad74dc324",
+    "url": "/static/js/main.9511ea35.chunk.js"
+  },
+  {
+    "revision": "b1d316aea5938fac9296",
+    "url": "/static/js/runtime-main.7d8baf8b.js"
+  }
+]);
